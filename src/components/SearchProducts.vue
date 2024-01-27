07e440fc0bdf35553373acc1f99b0225d231a043
thiago-NSampaio <110494178+thiago-NSampaio@users.noreply.github.com>
2024-01-27 18:54:29 -0400

    Estilizando os inputs

--- src/components/SearchProducts.vue
+++ src/components/SearchProducts.vue
@@ -1,6 +1,6 @@
 <template>
     <form>
-        <input name="search" id="search" type="text" v-model="search">
+        <input name="search" id="search" type="text" placeholder="Buscar..." v-model="search">
         <input type="submit" name="seek" id="find" @click.prevent="findProducts">
     </form>
 </template>
@@ -21,5 +21,36 @@ export default {
     }
 </script>
 
-<style>
+<style scoped>
+form{
+    max-width: 600px;
+    margin: 30px auto 60px auto;
+    position: relative;
+}
+
+#search{
+    width: 100%;
+    padding: 20px;
+    border: none;
+}
+
+#search:focus,
+#search:hover{
+    outline: none;
+    transform: scale(1.1);
+    box-shadow: 0 6px 12px rgba(30, 60, 90, 0.2);
+}
+
+#find{
+    width: 62px;
+    height: 62px;
+    background: url("../assets/search.svg") no-repeat center center;
+    text-indent: -150px;
+    cursor: pointer;
+    position: absolute;
+    border: none;
+    top: 0px;
+    right: 0px;
+    box-shadow:none;
+}
 </style>
